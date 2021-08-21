2035feb3b2a29f5f5389d84e88a78eb3f325b4a2
Alex Li <lialex107@gmail.com>
2021-08-21 12:35:01 -0400

    changed settings.py again

--- hackThe6ixProject/settings.py
+++ hackThe6ixProject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticFiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticFiles'))
 STATIC_ROOT = os.path.join(BASE_DIR, "staticFiles")
 
 # Default primary key field type
