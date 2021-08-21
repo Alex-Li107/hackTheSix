1409791bf53fa0be2407b940ca5fa83238265321
Alex Li <lialex107@gmail.com>
2021-08-21 12:04:12 -0400

    added some comments to views.py in goodJuJu

--- goodJuJu/views.py
+++ goodJuJu/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+#The code to excute when on the home page goes here
 def index(request):
     return HttpResponse('hello men')
 
+#When this will handle loading the code for the results page and ig processing the results of the survey 
 def results(request):
     return HttpResponse('hello mens')
