71d48414584d16d59d77b5235929e5efe6457de6
Alex Li <lialex107@gmail.com>
2021-08-21 12:15:59 -0400

    added homePage html and css and added that to the views

--- goodJuJu/views.py
+++ goodJuJu/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 #The code to excute when on the home page goes here
 def index(request):
-    return HttpResponse('hello men')
+    return render(request, '16Pathways.html')
 
 #When this will handle loading the code for the results page and ig processing the results of the survey 
 def results(request):
