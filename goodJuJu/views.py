9fc28e5e879b1789947e07e067265551b857c97d
Xiphyrus <wangeric2003@gmail.com>
2021-08-22 01:46:39 -0400

    views, landing2, and results.

--- goodJuJu/views.py
+++ goodJuJu/views.py
@@ -20,13 +20,149 @@ def results(request):
     if Sum >= 76:
         values_dict =  {
             "perty": "INFJ",
-            "job1": "test"
+            "cluster": "Anthropology, Sociology, & Psychology",
+            "job1": "Psychiatrist",
+            "job2": "Therapist",
+            "job3": "Social/Human Service Worker",
+            "job4": "Child Welfare Worker"
         }
-    elif Sum < 76 and Sum >= 71:
+    elif Sum >= 71 and Sum <= 75:
         values_dict =  {
             "perty": "ENFJ",
-            "job1": "testt"
+            "cluster": "Social Work",
+            "job1": "Counsellor",
+            "job2": "Family Mediator",
+            "job3": "Child Protection Services Worker",
+            "job4": "Human Rights Lawyer"
         }
+    elif Sum >= 66 and Sum <= 70:
+        values_dict =  {
+            "perty": "ISFJ",
+            "cluster": "Health Sciences ",
+            "job1": "Family Physician",
+            "job2": "Kinesiologist ",
+            "job3": "Nurse",
+            "job4": "Community Relations Officer"
+        }
+    elif Sum >= 61 and Sum <= 65:
+        values_dict =  {
+            "perty": "INTJ",
+            "cluster": "Computer Sciences",
+            "job1": "Data Scientist",
+            "job2": "Web Developer",
+            "job3": "Computer System Analysts",
+            "job4": "Software Engineer"
+        }
+    elif Sum >= 55 and Sum <= 60:
+        values_dict =  {
+            "perty": "INFP",
+            "cluster": "English & Writing Studies",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 50 and Sum <= 54:
+        values_dict =  {
+            "perty": "ENTJ",
+            "cluster": "Political Science",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 45 and Sum <= 49:
+        values_dict =  {
+            "perty": "ESFJ",
+            "cluster": "Education",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 40 and Sum <= 44:
+        values_dict =  {
+            "perty": "ENFP",
+            "cluster": "Media & Communication Studies",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 35 and Sum <= 39:
+        values_dict =  {
+            "perty": "ISTJ",
+            "cluster": "Engineering",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 30 and Sum <= 34:
+        values_dict =  {
+            "perty": "INTP",
+            "cluster": "Mathematics",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 25 and Sum <= 29:
+        values_dict =  {
+            "perty": "ISFP",
+            "cluster": "Creative Art & Design",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 20 and Sum <= 24:
+        values_dict =  {
+            "perty": "ISTP",
+            "cluster": "Athletics",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 15 and Sum <= 19:
+        values_dict =  {
+            "perty": "ENTP",
+            "cluster": "Physical Science",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 10 and Sum <= 14:
+        values_dict =  {
+            "perty": "ESFP",
+            "cluster": "Performing Arts",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 5 and Sum <= 9:
+        values_dict =  {
+            "perty": "ESTJ",
+            "cluster": "Law Enforcement",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    elif Sum >= 0 and Sum <= 4:
+        values_dict =  {
+            "perty": "ESTP",
+            "cluster": "Entrepreneurship & Business Studies ",
+            "job1": "",
+            "job2": "",
+            "job3": "",
+            "job4": ""
+        }
+    
+    
 
     
     return render(request, 'results.html', values_dict)
