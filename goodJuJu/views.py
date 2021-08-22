f73d7c0b49022df273a28b8bcd8b0845aaaf22c3
Alex Li <lialex107@gmail.com>
2021-08-21 21:48:51 -0400

    added comments

--- goodJuJu/views.py
+++ goodJuJu/views.py
@@ -10,13 +10,22 @@ def results(request):
     values = ""
     if request.method == 'POST':
         values = request.POST.getlist('val')
+        #values is a list of strings, change the values into an int and sum them up so aaren can find out ur personailty
         #for i in range (len(values)):
             #Sum = Sum + values[i]
-        print(values)
+        #you dont need this if u summing it all up. Delete when ur done
         values_dict = {
             'values': values
         }
+    
+    #if statements to determine personality
 
+
+    #create a dictionary with personailty and possible jobs
+    #values_dict = {
+        #.....
+    #}
+    
     return render(request, 'results.html', values_dict)
 
 def aboutus(request):
