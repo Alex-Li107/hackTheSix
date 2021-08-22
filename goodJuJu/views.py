4adf6d901b0772e11d6bee87f63a9165a0ee75d2
Alex Li <lialex107@gmail.com>
2021-08-21 21:13:26 -0400

    out like a light

--- goodJuJu/views.py
+++ goodJuJu/views.py
@@ -7,14 +7,17 @@ def index(request):
 
 #When this will handle loading the code for the results page and ig processing the results of the survey 
 def results(request):
-    Sum = 0
+    values = ""
     if request.method == 'POST':
         values = request.POST.getlist('val')
-        for i in range (len(values)):
-            Sum = Sum + values[i]
-    else:
-        values = {}
-    return render(request, 'results.html', {'num': values})
+        #for i in range (len(values)):
+            #Sum = Sum + values[i]
+        print(values)
+        values_dict = {
+            'values': values
+        }
+
+    return render(request, 'results.html', values_dict)
 
 def aboutus(request):
     return render(request, 'aboutUs.html')
